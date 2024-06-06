bc754e590013d630f76f0719c0a611246a137195
12108749 Sakhi Firjatullah <sakhifirjatullah@smkwikrama.sch.id>
2024-06-06 18:36:06 +0700

    navbar

--- index.php
+++ index.php
@@ -29,40 +29,44 @@ if(!isset($_SESSION['user'])) {
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <!-- Navbar Brand-->
-        <!-- Bagian "Login as" dan nama pengguna dipindahkan ke footer -->
-        
-        <!-- Navbar Search dan Tautan Menu di sini -->
-        <div class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-            <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><?php echo $_SESSION['user']['nama']?></a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <!-- Navbar Brand -->
+        <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo $_SESSION['user']['nama'] ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+
+        <!-- Navbar Toggler -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar Collapse -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=pelanggan_admin">User</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=produk">Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=pembelian_admin">Pembelian</a>
+                </li>
+            </ul>
+
+         
         </div>
-        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            <div class="input-group">
-                <a class="nav-link" href="index.php">
-                    <div class="sb-nav-link-icon"></div>
-                    Dashboard
-                </a> 
-                <a class="nav-link" href="?page=pelanggan_admin">
-                    <div class="sb-nav-link-icon"></div>
-                    User
-                </a>
-                <a class="nav-link" href="?page=produk">
-                    <div class="sb-nav-link-icon"></div>
-                    Produk
-                </a>
-                <a class="nav-link" href="?page=pembelian_admin">
-                    <div class="sb-nav-link-icon"></div>
-                    Pembelian
-                </a>
-            </div>
-        </form>
-    </nav>
+    </div>
+</nav>
             </nav>
         </div>
         <!-- buat memisahkan logika dan tampilan ke dalam file-file terpisah -->
